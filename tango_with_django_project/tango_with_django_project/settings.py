fdd46a218bf2a8778ba5a8ef6f0006f57ad4d160
Ruaridh Wilson <RuaridhSWilson@gmail.com>
2020-01-26 11:12:01 +0000

    added django-registration-redux

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -133,5 +134,12 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 
-# login_required() redirect destination
-LOGIN_URL = "rango:login"
+# Registration configuration
+
+REGISTRATION_OPEN = True
+
+REGISTRATION_AUTO_LOGIN = True
+
+LOGIN_REDIRECT_URL = "rango:index"
+
+LOGIN_URL = "auth_login"
